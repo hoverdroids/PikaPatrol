b2b3f1fa891a472f3906308baebec16ae2e432e5
Chris Sprague <spragucm@gmail.com>
2020-07-19 14:34:12 -0400

    Added Google Services Json. Seems to build
    - who knows yet

--- lib/main.dart
+++ lib/main.dart
@@ -109,6 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        backgroundColor: Colors.green,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.

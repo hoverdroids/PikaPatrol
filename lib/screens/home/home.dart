6ab1066d279ab4b88b44f8d3e0dae6529040f07f
Chris Sprague <spragucm@gmail.com>
2020-07-26 01:22:15 -0400

    Added images. Finished Form MVP

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -45,7 +45,16 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-        body: BrewList()
+
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/coffee_bg.png'),
+              fit: BoxFit.cover
+            ),
+          ),
+          child: BrewList()
+        )
       ),
     );
   }

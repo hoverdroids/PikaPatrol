9b5aece1b582c0948b65a29aedafc6bb44384c29
Chris Sprague <spragucm@gmail.com>
2020-08-05 21:38:37 -0400

    WIP - left menu box in place

--- lib/screens/liquid_swipe_wrapper.dart
+++ lib/screens/liquid_swipe_wrapper.dart
@@ -1,5 +1,6 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:pika_joe/screens/splash/page1.dart';
 import 'package:pika_joe/screens/splash/page2.dart';
@@ -12,28 +13,58 @@ class LiquidSwipeWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Size mediaQuery = MediaQuery.of(context).size;
+    double sidebarSize = mediaQuery.width * 0.65;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: Text('Pika Patrol'),//TODO - apply a style
       ),
-      body: LiquidSwipe(
-        pages: <Container>[
-          Page1(),
-          Page2(),
-          Page3(),
-        ],
-        enableLoop: true,
-        fullTransitionValue: 300,
-        enableSlideIcon: true,
-        waveType: WaveType.liquidReveal,
-        positionSlideIcon: 0.5,
-        liquidController: liquidController,
-        ignoreUserGestureWhileAnimating: true,
-        //TODO - onPageChangeCallback: pageChangeCallback,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors:[
+              Color.fromRGBO(255, 65, 108, 1.0),
+              Color.fromRGBO(255, 75, 73, 1.0)
+            ],
+          ),
+        ),
+        width: mediaQuery.width,
+        child: Stack(
+          children: <Widget>[
+            LiquidSwipe(
+              pages: <Container>[
+                Page1(),
+                Page2(),
+                Page3(),
+              ],
+              enableLoop: true,
+              fullTransitionValue: 300,
+              enableSlideIcon: true,
+              waveType: WaveType.liquidReveal,
+              positionSlideIcon: 0.5,
+              liquidController: liquidController,
+              ignoreUserGestureWhileAnimating: true,
+              //TODO - onPageChangeCallback: pageChangeCallback,
+            ),
+            SizedBox(
+              width:sidebarSize,
+              child: Stack(
+                children: <Widget>[
+                  CustomPaint(
+                    size: Size(sidebarSize, mediaQuery.height),
+                    painter: DrawerPainter(),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: CurvedNavigationBar(
-        color: Colors.blueAccent,
+        color: Colors.amber,
         backgroundColor: Colors.white,
         buttonBackgroundColor: Colors.blueAccent,
         height: 50,
@@ -52,5 +83,27 @@ class LiquidSwipeWrapper extends StatelessWidget {
         index: 1,
       ),
     );
+  }
+}
+
+//TODO - move!
+class DrawerPainter extends CustomPainter {
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.white..style = PaintingStyle.fill;
+    Path path = Path();
+    path.moveTo(-size.width, 0);
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(-size.width, size.height);
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }

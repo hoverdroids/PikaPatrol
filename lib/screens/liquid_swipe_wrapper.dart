7629c114cc444d881d9d76cff78ffeb6cbc278bd
Chris Sprague <spragucm@gmail.com>
2020-08-05 21:17:45 -0400

    WIP - Added bottom navigation bar

--- lib/screens/liquid_swipe_wrapper.dart
+++ lib/screens/liquid_swipe_wrapper.dart
@@ -1,24 +1,22 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:liquid_swipe/liquid_swipe.dart';
 import 'package:pika_joe/screens/splash/page1.dart';
 import 'package:pika_joe/screens/splash/page2.dart';
 import 'package:pika_joe/screens/splash/page3.dart';
-import 'package:pika_joe/screens/splash/pika_patrol_splash.dart';
-import 'package:pika_joe/styles/styles.dart';
 
 class LiquidSwipeWrapper extends StatelessWidget {
-  static const TextStyle boldStyle = TextStyle(
-    fontSize: 50.0,
-    color: Colors.black,
-    fontFamily: "Product Sans",
-    fontWeight: FontWeight.bold,
-  );
 
-//widget(child: new PikaPatrolSpash())
+  //TODO - https://github.com/iamSahdeep/liquid_swipe_flutter/blob/master/example/lib/main.dart
+  LiquidController liquidController = LiquidController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('Pika Patrol'),//TODO - apply a style
+      ),
       body: LiquidSwipe(
         pages: <Container>[
           Page1(),
@@ -30,6 +28,28 @@ class LiquidSwipeWrapper extends StatelessWidget {
         enableSlideIcon: true,
         waveType: WaveType.liquidReveal,
         positionSlideIcon: 0.5,
+        liquidController: liquidController,
+        ignoreUserGestureWhileAnimating: true,
+        //TODO - onPageChangeCallback: pageChangeCallback,
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.blueAccent,
+        backgroundColor: Colors.white,
+        buttonBackgroundColor: Colors.blueAccent,
+        height: 50,
+        items: <Widget>[
+          Icon(Icons.add, size: 30),
+          Icon(Icons.list, size: 30),
+          Icon(Icons.compare_arrows, size: 30),
+        ],
+        onTap: (index) {
+          //TODO - liquidController.animateToPage(page: 2);//liquidController.currentPage + 1, duration: 500);
+        },
+        animationDuration: Duration(
+          milliseconds: 200
+        ),
+        animationCurve: Curves.bounceInOut,
+        index: 1,
       ),
     );
   }

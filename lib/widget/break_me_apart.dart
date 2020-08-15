606d5559193689f56739a1a71af0fd8d2745be7c
Chris Sprague <spragucm@gmail.com>
2020-08-15 15:53:54 -0400

    Scrolling cards and then clicking to open video details

--- lib/widget/break_me_apart.dart
+++ lib/widget/break_me_apart.dart
@@ -145,10 +145,12 @@ class _MyAppState2 extends State<MyApp2> {
                         allowImplicitScrolling: true,
                         itemBuilder: (context, index) {
                           return GestureDetector(
-                            onTap: (){
-                              final snackbar = SnackBar(content: Text('Clicking a page'));
-                              Scaffold.of(context).showSnackBar(snackbar);
-                            },
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => MovieScreen(movie: movies[2]),
+                              ),
+                            ),
                             child: Container(
                               width: double.infinity,
                               height: double.infinity,
@@ -287,16 +289,80 @@ class CardScrollWidget extends StatelessWidget {
             bottom: padding + verticalInset * max(-delta, 0.0),
             start: start,
             textDirection: TextDirection.rtl,
-            child: GestureDetector(
-              onTap: () {
-                final snackbar = SnackBar(content: Text("Wubbda"),);
-                Scaffold.of(context).showSnackBar(snackbar);
-              },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16.0),
               child: Container(
-                  width: 300,
-                  height: 300,
-                  color: Colors.red,
-                  child: Text("Page Text")),
+                decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(3.0, 6.0),
+                      blurRadius: 10.0)
+                ]),
+                child: AspectRatio(
+                  aspectRatio: cardAspectRatio,
+                  child: GestureDetector(
+                    onTap: () => print('Damnit'),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Image.asset(images[i], fit: BoxFit.cover),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 16.0, vertical: 8.0),
+                                child: Text(title[i],
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25.0,
+                                        fontFamily: "SF-Pro-Text-Regular")),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 12.0, bottom: 12.0),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 22.0, vertical: 6.0),
+                                  decoration: BoxDecoration(
+                                      color: Colors.blueAccent,
+                                      borderRadius: BorderRadius.circular(20.0)),
+                                  child: RawGestureDetector(
+                                    gestures: <Type, GestureRecognizerFactory>{
+                                      CustomPanGestureRecognizer: GestureRecognizerFactoryWithHandlers<CustomPanGestureRecognizer>(
+                                            () => CustomPanGestureRecognizer(
+                                            onPanDown: () => _onPanDown,
+                                            onPanUpdate: () => _onPanUpdate,
+                                            onPanEnd:  () => _onPanEnd
+                                        ),
+                                            (CustomPanGestureRecognizer instance) {},
+                                      ),
+                                    },
+                                    /*onTap: () => Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => MovieScreen(movie: movies[2]),
+                                      ),
+                                    ),*/
+                                    child: Text("Read Later",
+                                        style: TextStyle(color: Colors.white)),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           );
           cardList.add(cardItem);

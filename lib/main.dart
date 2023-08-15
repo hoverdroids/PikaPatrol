9c0c419efcce7204d789c927aca17e5bc4373ba0
Chris Sprague <spragucm@gmail.com>
2023-08-15 05:14:10 -0700

    WIP - added firebase dependencies based on instructions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  //https://codewithandrea.com/articles/flutter-firebase-flutterfire-cli/
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 

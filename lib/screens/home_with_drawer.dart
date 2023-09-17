3e3e4597377c9a17e599e935dfb3c78fdef28078
Chris Sprague <spragucm@gmail.com>
2023-09-16 17:38:48 -0700

    WIP - extracted builder functions from end drawer

--- lib/screens/home_with_drawer.dart
+++ lib/screens/home_with_drawer.dart
@@ -245,211 +245,219 @@ class HomeWithDrawerState extends State<HomeWithDrawer> {
               alignment: Alignment.center,
               children: <Widget>[
                 if (user != null) ... [
-                  StreamBuilder<AppUserProfile>(
-                      stream: FirebaseDatabaseService(uid: user.uid).userProfile,
-                      builder: (context, snapshot){
-
-                        var snapshotData = snapshot.data;
-                        AppUserProfile? userProfile = snapshot.hasData && snapshotData != null ? snapshotData : null;
-
-                        var editProfileKey = userProfile == null ? _editProfileNullKey : _editProfileKey;
-                        var viewProfileKey = userProfile == null ? _nullProfileKey : _profileKey;
-
-                        return ProfileScreen(
-                          //_nullProfileKey and _profileKey need to be different or else the ProfileScreen will not update without first receiving user input
-                          //also, one key for null and one for not null because, without the distinction, and if we use a new uniqueKey each time, the keyboard
-                          //pops up and then immediately pops back down when trying to type text
-                          key: isEditingProfile ? editProfileKey : viewProfileKey,
-                          isEditMode: isEditingProfile,
-                          onTapLogout: () async {
-                            setState(() { showSignIn = true; });//makes more sense to show signIn than register after signOut
-                            await firebaseAuthService.signOut();
-                          },
-                          onTapEdit: () => setState(() => isEditingProfile = true),
-                          onTapSave: () async {
-                            setState(() => loading = true);
-                            await Provider.of<FirebaseDatabaseService>(context).updateUserProfile(
-                                firstName ?? userProfile?.firstName ?? "NO USER PROFILE",
-                                lastName ?? userProfile?.lastName ?? "",
-                                tagline ?? userProfile?.tagline ?? "",
-                                pronouns ?? userProfile?.pronouns ?? "",
-                                organization ?? userProfile?.organization ?? "",
-                                address ?? userProfile?.address ?? "",
-                                city ?? userProfile?.city ?? "",
-                                state ?? userProfile?.state ?? "",
-                                zip ?? userProfile?.zip ?? "",
-                                frppOptIn ?? userProfile?.frppOptIn ?? false,
-                                rmwOptIn ?? userProfile?.rmwOptIn ?? false,
-                                dzOptIn ?? userProfile?.dzOptIn ?? false
-                            );
-                            setState(() => isEditingProfile = false);
-                            setState(() => loading = false);
-                          },
-                          onTapDelete: () async {
-                            Widget okButton = TextButton(
-                              child: const Text("OK"),
-                              onPressed:  () async {
-                                //Hide the alert
-                                Navigator.pop(context, true);
-
-                                setState(() { showSignIn = true; });//makes more sense to show signIn than register after signOut
-
-                                await firebaseAuthService.deleteUser();
-
-                                // Don't sign out before deleting user because the user must be signed into to delete themselves
-                                await firebaseAuthService.signOut();
-                                showToast("Account Deleted");
-                              },
-                            );
-
-                            AlertDialog alert = AlertDialog(
-                              title: const Text("Delete Account"),
-                              content: const Text("Are you sure you want to delete your account.? This cannot be undone. Your uploaded observations will remain on the server. Local observations that have not been uploaded will be removed from your device."),
-                              actions: [
-                                okButton,
-                              ],
-                            );
-
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return alert;
-                              },
-                            );
-                          },
-                          showEmail: false, //TODO - need to wait until we allow the user to change their email
-                          showPassword: false, //TODO - need to wait until we allow the user to change their password
-                          firstName: userProfile != null ? userProfile.firstName : "" ,
-                          lastName: userProfile != null ? userProfile.lastName : "",
-                          tagline: userProfile != null ? userProfile.tagline : "",
-                          pronouns: userProfile != null ? userProfile.pronouns : "",
-                          organization: userProfile != null ? userProfile.organization : "",
-                          address: userProfile != null ? userProfile.address : "",
-                          city: userProfile != null ? userProfile.city : "",
-                          state: userProfile != null ? userProfile.state : "",
-                          zip: userProfile != null ? userProfile.zip : "",
-                          onEmailChangedCallback: (value) => { email = value },
-                          onPasswordChangedCallback: (value) => { password = value },
-                        );
-                      }
-                  )
+                  buildProfileScreen(firebaseAuthService, userProfile),
                 ] else if(showSignIn) ... [
-                  LoginRegisterScreen(
-                    key: _loginKey,
-                    isLogin: true,
-                    showLabels: false,
-                    onPasswordChangedCallback: (value) => { password = value },
-                    onEmailChangedCallback: (value) => { email = value },
-                    onTapLogin: () async {
-
-                      var trimmedEmail = email?.trim() ?? "";
-                      var trimmedPassword = password?.trim() ?? "";
-
-                      if (trimmedEmail.isEmpty) {
-                        showToast("Email cannot be empty");
-                        return;
-                      } else if (trimmedPassword.isEmpty) {
-                        showToast("Password cannot be empty");
-                        return;
-                      }
-
-                      setState(() => loading = true);
-
-                      dynamic result = await firebaseAuthService.signInWithEmailAndPassword(trimmedEmail, trimmedPassword);
-                      //dynamic result = await _auth.signInWithGoogle();
-
-                      if(result == null) {
-                        // Need to determine if this was because there is no internet or if the sign in really wasn't accepted
-                        try {
-                          final result = await InternetAddress.lookup('google.com');
-                          if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-                            showToast("Could not sign in with those credentials");
-                          }
-                        } on SocketException catch (_) {
-                          showToast("Can not sign in. Not connected to internet.");
-                        }
-                      } else {
-                        showToast("Successfully Logged In");
-                      }
-
-                      setState((){ loading = false; });
-                    },
-                    onTapRegister: () => {
-                      setState(() => showSignIn = false)
-                    },
-                  ),
+                  buildLoginScreen(firebaseAuthService),
                 ] else ... [
-                  LoginRegisterScreen(
-                    key: _registerKey,
-                    isLogin: false,
-                    showLabels: false,
-                    onPasswordChangedCallback: (value) => { password = value },
-                    onEmailChangedCallback: (value) => { email = value },
-                    onTapLogin: () => { setState(() => showSignIn = true) },
-                    onTapRegister: () async {
-
-                      var trimmedEmail = email?.trim() ?? "";
-                      var trimmedPassword = password?.trim() ?? "";
-                      var trimmedFirstName = firstName?.trim() ?? "";
-                      var trimmedLastName = lastName?.trim() ?? "";
-                      var trimmedZip = zip?.trim() ?? "";
-
-                      if (trimmedEmail.isEmpty ||
-                          trimmedPassword.isEmpty ||
-                          trimmedFirstName.isEmpty ||
-                          trimmedLastName.isEmpty ||
-                          trimmedZip.isEmpty
-                      ) {
-                        return;
-                      }
-
-                      setState(() => loading = true);
-
-                      dynamic result = await firebaseAuthService.registerWithEmailAndPassword(
-                          trimmedEmail,
-                          trimmedPassword,
-                          trimmedFirstName,
-                          trimmedLastName,
-                          tagline ?? "",
-                          pronouns ?? "",
-                          organization ?? "",
-                          address ?? "",
-                          city ?? "",
-                          state ?? "",
-                          zip ?? "",
-                          frppOptIn ?? false,
-                          rmwOptIn ?? false,
-                          dzOptIn ?? false
-                      );
-
-                      if(result == null) {
-                        // Need to determine if this was because there is no internet or if the sign in really wasn't accepted
-                        try {
-                          final result = await InternetAddress.lookup('google.com');
-                          if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
-                            showToast("Could not register with those credentials");
-                          }
-                        } on SocketException catch (_) {
-                          showToast("Can not register. Not connected to internet.");
-                        }
-                      } else {
-                        showToast("Successfully Registered");
-                      }
-                      setState(() => loading = false);
-                    },
-                  ),
+                  buildRegisterScreen(firebaseAuthService),
                 ],
                 if(loading) ... [
-                  Container(
-                    width: double.infinity,
-                    height: double.infinity,
-                    color: Colors.white.withOpacity(0.70),
-                    child: Loading(),
-                  )
+                  buildLoadingOverlay()
                 ]
               ],
             )
         )
+    );
+  }
+
+  Widget buildProfileScreen(FirebaseAuthService firebaseAuthService, AppUserProfile? userProfile) {
+
+    var editProfileKey = userProfile == null ? _editProfileNullKey : _editProfileKey;
+    var viewProfileKey = userProfile == null ? _nullProfileKey : _profileKey;
+
+    return ProfileScreen(
+      //_nullProfileKey and _profileKey need to be different or else the ProfileScreen will not update without first receiving user input
+      //also, one key for null and one for not null because, without the distinction, and if we use a new uniqueKey each time, the keyboard
+      //pops up and then immediately pops back down when trying to type text
+      key: isEditingProfile ? editProfileKey : viewProfileKey,
+      isEditMode: isEditingProfile,
+      onTapLogout: () async {
+        setState(() { showSignIn = true; });//makes more sense to show signIn than register after signOut
+        await firebaseAuthService.signOut();
+      },
+      onTapEdit: () => setState(() => isEditingProfile = true),
+      onTapSave: () async {
+        setState(() => loading = true);
+        await Provider.of<FirebaseDatabaseService>(context).updateUserProfile(
+            firstName ?? userProfile?.firstName ?? "NO USER PROFILE",
+            lastName ?? userProfile?.lastName ?? "",
+            tagline ?? userProfile?.tagline ?? "",
+            pronouns ?? userProfile?.pronouns ?? "",
+            organization ?? userProfile?.organization ?? "",
+            address ?? userProfile?.address ?? "",
+            city ?? userProfile?.city ?? "",
+            state ?? userProfile?.state ?? "",
+            zip ?? userProfile?.zip ?? "",
+            frppOptIn ?? userProfile?.frppOptIn ?? false,
+            rmwOptIn ?? userProfile?.rmwOptIn ?? false,
+            dzOptIn ?? userProfile?.dzOptIn ?? false
+        );
+        setState(() => isEditingProfile = false);
+        setState(() => loading = false);
+      },
+      onTapDelete: () async {
+        Widget okButton = TextButton(
+          child: const Text("OK"),
+          onPressed:  () async {
+            //Hide the alert
+            Navigator.pop(context, true);
+
+            setState(() { showSignIn = true; });//makes more sense to show signIn than register after signOut
+
+            await firebaseAuthService.deleteUser();
+
+            // Don't sign out before deleting user because the user must be signed into to delete themselves
+            await firebaseAuthService.signOut();
+            showToast("Account Deleted");
+          },
+        );
+
+        AlertDialog alert = AlertDialog(
+          title: const Text("Delete Account"),
+          content: const Text("Are you sure you want to delete your account.? This cannot be undone. Your uploaded observations will remain on the server. Local observations that have not been uploaded will be removed from your device."),
+          actions: [
+            okButton,
+          ],
+        );
+
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return alert;
+          },
+        );
+      },
+      showEmail: false, //TODO - need to wait until we allow the user to change their email
+      showPassword: false, //TODO - need to wait until we allow the user to change their password
+      firstName: userProfile != null ? userProfile.firstName : "" ,
+      lastName: userProfile != null ? userProfile.lastName : "",
+      tagline: userProfile != null ? userProfile.tagline : "",
+      pronouns: userProfile != null ? userProfile.pronouns : "",
+      organization: userProfile != null ? userProfile.organization : "",
+      address: userProfile != null ? userProfile.address : "",
+      city: userProfile != null ? userProfile.city : "",
+      state: userProfile != null ? userProfile.state : "",
+      zip: userProfile != null ? userProfile.zip : "",
+      onEmailChangedCallback: (value) => { email = value },
+      onPasswordChangedCallback: (value) => { password = value },
+    );
+  }
+
+  Widget buildLoginScreen(FirebaseAuthService firebaseAuthService) {
+    return LoginRegisterScreen(
+      key: _loginKey,
+      isLogin: true,
+      showLabels: false,
+      onPasswordChangedCallback: (value) => { password = value },
+      onEmailChangedCallback: (value) => { email = value },
+      onTapLogin: () async {
+
+        var trimmedEmail = email?.trim() ?? "";
+        var trimmedPassword = password?.trim() ?? "";
+
+        if (trimmedEmail.isEmpty) {
+          showToast("Email cannot be empty");
+          return;
+        } else if (trimmedPassword.isEmpty) {
+          showToast("Password cannot be empty");
+          return;
+        }
+
+        setState(() => loading = true);
+
+        dynamic result = await firebaseAuthService.signInWithEmailAndPassword(trimmedEmail, trimmedPassword);
+        //dynamic result = await _auth.signInWithGoogle();
+
+        if(result == null) {
+          // Need to determine if this was because there is no internet or if the sign in really wasn't accepted
+          try {
+            final result = await InternetAddress.lookup('google.com');
+            if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+              showToast("Could not sign in with those credentials");
+            }
+          } on SocketException catch (_) {
+            showToast("Can not sign in. Not connected to internet.");
+          }
+        } else {
+          showToast("Successfully Logged In");
+        }
+
+        setState((){ loading = false; });
+      },
+      onTapRegister: () => {
+        setState(() => showSignIn = false)
+      },
+    );
+  }
+
+  Widget buildRegisterScreen(FirebaseAuthService firebaseAuthService) {
+    return LoginRegisterScreen(
+      key: _registerKey,
+      isLogin: false,
+      showLabels: false,
+      onPasswordChangedCallback: (value) => { password = value },
+      onEmailChangedCallback: (value) => { email = value },
+      onTapLogin: () => { setState(() => showSignIn = true) },
+      onTapRegister: () async {
+
+        var trimmedEmail = email?.trim() ?? "";
+        var trimmedPassword = password?.trim() ?? "";
+        var trimmedFirstName = firstName?.trim() ?? "";
+        var trimmedLastName = lastName?.trim() ?? "";
+        var trimmedZip = zip?.trim() ?? "";
+
+        if (trimmedEmail.isEmpty ||
+            trimmedPassword.isEmpty ||
+            trimmedFirstName.isEmpty ||
+            trimmedLastName.isEmpty ||
+            trimmedZip.isEmpty
+        ) {
+          return;
+        }
+
+        setState(() => loading = true);
+
+        dynamic result = await firebaseAuthService.registerWithEmailAndPassword(
+            trimmedEmail,
+            trimmedPassword,
+            trimmedFirstName,
+            trimmedLastName,
+            tagline ?? "",
+            pronouns ?? "",
+            organization ?? "",
+            address ?? "",
+            city ?? "",
+            state ?? "",
+            zip ?? "",
+            frppOptIn ?? false,
+            rmwOptIn ?? false,
+            dzOptIn ?? false
+        );
+
+        if(result == null) {
+          // Need to determine if this was because there is no internet or if the sign in really wasn't accepted
+          try {
+            final result = await InternetAddress.lookup('google.com');
+            if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+              showToast("Could not register with those credentials");
+            }
+          } on SocketException catch (_) {
+            showToast("Can not register. Not connected to internet.");
+          }
+        } else {
+          showToast("Successfully Registered");
+        }
+        setState(() => loading = false);
+      },
+    );
+  }
+
+  Widget buildLoadingOverlay() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Colors.white.withOpacity(0.70),
+      child: Loading(),
     );
   }
 

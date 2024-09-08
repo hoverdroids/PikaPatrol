e83b4800e8f64726a700d82199d943935c0505c8
Chris Sprague <spragucm@gmail.com>
2024-09-07 19:36:13 -0700

    WIP - spreadsheet service passthroughs for completeness

---
+++ lib/model/gsheets_value.dart
@@ -0,0 +1,9 @@
+import 'package:gsheets/gsheets.dart';
+
+class GSheetsValue<T> {
+
+  T? value;
+  GSheetsException? exception;
+
+  GSheetsValue(this.value, {this.exception});
+}
--- lib/services/pika_patrol_spreadsheet_service.dart
+++ lib/services/pika_patrol_spreadsheet_service.dart
@@ -1,3 +1,4 @@
+import 'package:gsheets/gsheets.dart';
 import 'package:pika_patrol/services/spreadsheet_service.dart';
 import 'package:pika_patrol/services/user_profiles_worksheet_service.dart';
 import 'observations_worksheet_service.dart';
@@ -15,7 +16,7 @@ class PikaPatrolSpreadsheetService extends SpreadsheetService {
   );
 
   @override
-  void initWorksheetServices(bool doInitHeaderRow, int columnHeadersRowNumber) {
+  void initWorksheetServices(Spreadsheet spreadsheet, bool doInitHeaderRow, int columnHeadersRowNumber) {
     userProfilesWorksheetService = UserProfilesWorksheetService(spreadsheet, doInitHeaderRow, columnHeadersRowNumber: columnHeadersRowNumber);
     worksheetServices.add(userProfilesWorksheetService!);
 
